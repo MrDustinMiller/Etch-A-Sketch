fb44f835b9037e6413143e4a5dcb17bf23eb4bd5
MrDustinMiller <dustin.miller@mscoding.org>
2022-10-29 23:38:24 -0500

    fix drawing, add erase, add rdnm colors, and shake feature

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,92 @@
 const container = document.querySelector(".container");
-const newGridBtn = document.querySelector(".newGridBtn");
+const resizeBtn = document.querySelector(".changeGridSize");
+const eraseBtn = document.querySelector(".eraseColor");
+const changeColorBtn = document.querySelector(".changeColor");
+const rndmColorBtn = document.querySelector(".randomColor");
 
-window.addEventListener("pageshow", buildBoard(256));
+window.addEventListener("pageshow", createGrid(16, 16));
 
-function buildBoard(size) {
-  for (let i = 0; i < size; i++) {
+function createGrid(col, rows) {
+  for (let i = 0; i < col * rows; i++) {
     const gameBoard = document.createElement("div");
-    container.appendChild(gameBoard);
-    gameBoard.setAttribute("class", "div1");
-    gameBoard.classList.add("div1");
-    changeDivColor(gameBoard);
+    container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    container.appendChild(gameBoard).classList.add("gameBoard");
+    gameBoard.addEventListener("mouseover", startingColor);
   }
 }
 
-function changeDivColor(gameBoard) {
-  gameBoard.addEventListener("mouseover", () => {
-    gameBoard.style.backgroundColor = "red";
+function startingColor(e) {
+  e.target.style.backgroundColor = "red";
+}
+
+function changeDivColor(colorString) {
+  const gameBoard = document.querySelectorAll(".gameBoard");
+
+  gameBoard.forEach((gameBoard) => {
+    gameBoard.addEventListener("mouseover", () => {
+      gameBoard.style.backgroundColor = `${colorString}`;
+    });
   });
 }
 
-const gridSize = newGridBtn.addEventListener("click", () => {
-  let size = prompt(
-    "How many number of squares per side for the new grid would you like?"
-  );
+let deleteGrid = () => {
+  let gridItems = document.querySelectorAll(".gameBoard");
+  gridItems.forEach((gridItems) => {
+    container.removeChild(gridItems);
+  });
+};
 
-  if (size > 100) {
-    prompt("Please enter a number less than 100!");
+let eraseSqaure = () => {
+  let gridItem = document.querySelectorAll(".gameBoard");
+  let body = document.querySelector("body");
+
+  gridItem.forEach((gridItems) => {
+    gridItems.style.backgroundColor = "rgb(240,255,240)";
+    body.classList.add("shake");
+  });
+
+  setTimeout(() => {
+    location.reload();
+  }, "1030");
+};
+
+let changeColor = () => {
+  const color = document.querySelector(".color");
+
+  color.addEventListener("input", (e) => {
+    color.style.backgroundColor = color.value;
+    let colorString = color.style.backgroundColor;
+    changeDivColor(colorString);
+  });
+};
+
+let rndmColor = () => {
+  const gameBoard = document.querySelectorAll(".gameBoard");
+
+  gameBoard.forEach((gameBoard) => {
+    gameBoard.addEventListener("mouseover", () => {
+      let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+      gameBoard.style.backgroundColor = `${randomColor}`;
+    });
+  });
+};
+
+resizeBtn.addEventListener("click", () => {
+  let size = prompt("How many squares per side would you like?");
+
+  if (size == "") {
+    createGrid(16, 16);
+  } else if (size > 100) {
+    size = prompt("Sorry but thats too high, choose a number lower than 100!");
+  } else if (size < 0) {
+    size = prompt("Sorry but thats too low, choose a number higher than 0!");
+  } else {
+    deleteGrid();
+    createGrid(size, size);
   }
-
-  let newSize = size * size;
-  buildBoard(Number(newSize));
-  //problem is the size is being added to the column height and not width.
-  //need both? needs to stay in same total space regardless of grid size
-  //maybe buldboard function is standlaone to build board on page load
-  //then second modifyBoard function to modify said board based on user inputs
 });
+
+eraseBtn.addEventListener("click", eraseSqaure);
+changeColorBtn.addEventListener("click", changeColor);
+rndmColorBtn.addEventListener("click", rndmColor);
